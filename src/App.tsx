c368fa5096756300f7ccaf9c728e5c3858e3dcbe
[dyad] <hareldikshtain1@gmail.com>
2026-06-10 21:11:54 +0300

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,25 @@
-"use client"; import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; import { BrowserRouter, Routes, Route } from "react-router-dom"; import Home from "./pages/Home"; import Game from "./pages/Game"; import NotFound from "./pages/NotFound"; const queryClient = new QueryClient(); const App = () => ( <QueryClientProvider client={queryClient}> <BrowserRouter> <Routes> <Route path="/" element={<Home />} /> <Route path="/game" element={<Game />} /> <Route path="*" element={<NotFound />} /> </Routes> </BrowserRouter> </QueryClientProvider> ); export default App;
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+import NotFound from "./pages/NotFound";
+import Simulator from "./pages/Simulator";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/simulator" element={<Simulator />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
+);
+
+export default App;
